7129909a81a08a82d03de1ff262c329c0c2895e3
Srivastava <Abhinav-X.Srivastava@analog.com>
2026-07-05 18:40:12 +0530

    Home logo: inline monochrome purple mic on same line as JamAlong

--- src/Home.jsx
+++ src/Home.jsx
@@ -57,7 +57,13 @@ export default function Home({ onEnterRoom }) {
   return (
     <div className="home">
       <div className="hero">
-        <h1>🎤 JamAlong</h1>
+        <h1>
+          <svg className="brand-mic" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
+            <path d="M12 15a3 3 0 0 0 3-3V6a3 3 0 0 0-6 0v6a3 3 0 0 0 3 3z" />
+            <path d="M19 11a1 1 0 1 0-2 0 5 5 0 0 1-10 0 1 1 0 1 0-2 0 7 7 0 0 0 6 6.92V21H8a1 1 0 1 0 0 2h8a1 1 0 1 0 0-2h-3v-3.08A7 7 0 0 0 19 11z" />
+          </svg>
+          JamAlong
+        </h1>
       </div>
 
       <div className="name-field">
